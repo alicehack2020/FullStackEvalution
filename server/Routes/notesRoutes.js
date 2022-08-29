b5fe43113812dbaa8e5f0a7109c627b8849527ff
Mangesh2042 <mangesh2042@gmail.com>
2022-08-29 20:36:16 +0530

    server code

---
+++ server/Routes/notesRoutes.js
@@ -0,0 +1,7 @@
+const note=require("express").Router()
+const NoteControllers=require("../controllers/NoteControllers")
+
+note.post("/addNotes",NoteControllers.addNotes)
+note.get("/getNotes",NoteControllers.getNotes)   
+note.delete("/noteDelete",NoteControllers.noteDelete)   
+module.exports=note;
---
+++ server/controllers/NoteControllers.js
@@ -0,0 +1,33 @@
+const mongoose=require("mongoose")
+const NoteModel=require("../model/notes")
+
+class NoteControllers{
+
+    static getNotes=async(req,res)=>
+    {
+        const note=await NoteModel.find()
+        res.send(note)
+    }
+
+    static addNotes=async (req,res)=>
+    {
+        const note=await NoteModel.create({
+                "taskname":req.body.taskname ,
+                "status":req.body.status ,
+                "tag":req.body.tag ,
+                 
+        })
+        res.send(note)
+    }
+
+    static noteDelete=async(req,res)=>{
+        const {_id}=req.body
+        const id=_id
+        const note=await NoteModel.deleteOne({"_id":id})
+        res.send(note)
+    }
+
+ 
+}
+
+module.exports=NoteControllers;
